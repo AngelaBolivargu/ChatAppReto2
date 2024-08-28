a54f4ba2b99fe63853dc7b1603474917808ed158
AngelaBolivargu <151105375+AngelaBolivargu@users.noreply.github.com>
2024-08-27 20:07:40 -0500

    Add files via upload

---
+++ UserListPresenter.java
@@ -0,0 +1,6 @@
+package com.example.chatappreto2;
+
+//[Interfaz para el presentador de la lista de usuarios]
+public interface UserListPresenter {
+    void loadUsers(); // Método para cargar la lista de usuarios
+}
